1e47765bd9fc9c63ec2da412c34dc0a278367e35
EARLE HARSHITH NAIDU <322103310062@gvpce.ac.in>
2026-03-10 01:10:36 +0530

    vb

--- backend/main.py
+++ backend/main.py
@@ -43,7 +43,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("CLIENT_URL", "http://localhost:3000")],
+    allow_origins=["*"],  # for development
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
